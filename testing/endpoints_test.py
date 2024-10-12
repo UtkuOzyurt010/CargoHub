6b8e741f2bcb28f93d689410315b8e69c559e61b
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-10-12 13:58:27 +0200

    cleaned / updated comments

--- testing/endpoints_test.py
+++ testing/endpoints_test.py
@@ -1,12 +1,10 @@
 import requests
 import json
-import os
+import os # for making directories and reading their files
 import shutil # for copying files
 import datetime
 import string
 from timeit import default_timer as timer
-
-import pdb
 
 def remove_timestamps(obj):
     newobj = dict.copy(obj)
@@ -43,168 +41,10 @@ def test_get():
             with open(f"testing/results/GET/{test_datetime}/{results_file_name}.json", "w") as f:
                 json.dump(diagnostics[results_file_name], f)
             #assert response.ok
-            #assert success
+            #assert success #keep this disabled until GET tests are seperated, 
+            #otherwise testing might stop before all endpoints are tested
 
 
-#make it so that this can be called for each endpoint, so the tests are ran seperately,
-# rather than in a for loop
-# def test_post():
-#     test_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#     os.makedirs("testing/results/POST/" + test_datetime)
-#     diagnostics = {}
-#     post_objects = {}
-#     files = os.listdir("data")
-#     for file in files: #e.g. inventories.json 
-#         post_objects[file.split(".")[0]] = {} #set up dictionary for objects
-#     for obj in post_objects: #populate dictionary with test objects
-#         match obj:
-#             case "clients":#todo
-#                 post_objects[obj] = {"id": 999, "name": "Test_Client", "address": "Nowhere", "city": "Nowhere", "zip_code": "00000", "province": "Nowhere", "country": "Nowhere", "contact_name": "Test_Client", "contact_phone": "242.732.3483x2573", "contact_email": "test_client@example.net", "created_at": "", "updated_at": ""}
-#             case "inventories":#todo
-#                 post_objects[obj] = {"id": 999, "item_id": "P000001", "description": "Face-to-face clear-thinking complexity", "item_reference": "sjQ23408K", "locations": [3211, 24700, 14123, 19538, 31071, 24701, 11606, 11817], "total_on_hand": 262, "total_expected": 0, "total_ordered": 80, "total_allocated": 41, "total_available": 141, "created_at": "", "updated_at": ""}
-#             case "item_groups":#todo
-#                 post_objects[obj] = {"id": 999, "name": "Electronics", "description": "", "created_at": "", "updated_at": ""}
-#             case "item_lines":#todo
-#                 post_objects[obj] = {"id": 999, "name": "Tech Gadgets", "description": "", "created_at": "", "updated_at": ""}
-#             case "item_types":#todo
-#                 post_objects[obj] = {"id": 999, "name": "Laptop", "description": "", "created_at": "", "updated_at": ""}
-#             case "items":#todo
-#                 post_objects[obj] = {
-#                     "uid": "999",
-#                     "code": "sjQ23408K",
-#                     "description": "Face-to-face clear-thinking complexity",
-#                     "short_description": "must",
-#                     "upc_code": "6523540947122",
-#                     "model_number": "63-OFFTq0T",
-#                     "commodity_code": "oTo304",
-#                     "item_line": 11,
-#                     "item_group": 73,
-#                     "item_type": 14,
-#                     "unit_purchase_quantity": 47,
-#                     "unit_order_quantity": 13,
-#                     "pack_order_quantity": 11,
-#                     "supplier_id": 34,
-#                     "supplier_code": "SUP423",
-#                     "supplier_part_number": "E-86805-uTM",
-#                     "created_at": "",
-#                     "updated_at": ""
-#                 }
-#             case "locations":#todo
-#                 post_objects[obj] = {"id": 999, "warehouse_id": 1, "code": "A.1.0", "name": "Row: A, Rack: 1, Shelf: 0", "created_at": "", "updated_at": ""}
-#             case "orders":#todo
-#                 post_objects[obj] = {
-#                     "id": 999,
-#                     "source_id": 33,
-#                     "order_date": "2019-04-03T11:33:15Z",
-#                     "request_date": "2019-04-07T11:33:15Z",
-#                     "reference": "ORD00001",
-#                     "reference_extra": "Bedreven arm straffen bureau.",
-#                     "order_status": "Delivered",
-#                     "notes": "Voedsel vijf vork heel.",
-#                     "shipping_notes": "Buurman betalen plaats bewolkt.",
-#                     "picking_notes": "Ademen fijn volgorde scherp aardappel op leren.",
-#                     "warehouse_id": 18,
-#                     "ship_to": None, #supposed to be null
-#                     "bill_to": None, #supposed to be null
-#                     "shipment_id": 1,
-#                     "total_amount": 9905.13,
-#                     "total_discount": 150.77,
-#                     "total_tax": 372.72,
-#                     "total_surcharge": 77.6,
-#                     "created_at": "",
-#                     "updated_at": "",
-#                     "items": [] #todo
-#                 }
-#             case "shipments":#todo
-#                 post_objects[obj] =  {
-#                     "id": 999,
-#                     "order_id": 1,
-#                     "source_id": 33,
-#                     "order_date": "2000-03-09",
-#                     "request_date": "2000-03-11",
-#                     "shipment_date": "2000-03-13",
-#                     "shipment_type": "I",
-#                     "shipment_status": "Pending",
-#                     "notes": "Zee vertrouwen klas rots heet lachen oneven begrijpen.",
-#                     "carrier_code": "DPD",
-#                     "carrier_description": "Dynamic Parcel Distribution",
-#                     "service_code": "Fastest",
-#                     "payment_type": "Manual",
-#                     "transfer_mode": "Ground",
-#                     "total_package_count": 31,
-#                     "total_package_weight": 594.42,
-#                     "created_at": "",
-#                     "updated_at": "",
-#                     "items": [] #todo
-#                 }
-#             case "suppliers":#todo
-#                 post_objects[obj] = {"id": 999, "code": "SUP0001", "name": "Lee, Parks and Johnson", "address": "5989 Sullivan Drives", "address_extra": "Apt. 996", "city": "Port Anitaburgh", "zip_code": "91688", "province": "Illinois", "country": "Czech Republic", "contact_name": "Toni Barnett", "phonenumber": "363.541.7282x36825", "reference": "LPaJ-SUP0001", "created_at": "", "updated_at": ""}
-#             case "transfers":#todo
-#                 post_objects[obj] = {
-#                     "id": 999,
-#                     "reference": "TR00001",
-#                     "transfer_from": None, #supposed to be null
-#                     "transfer_to": 9229,
-#                     "transfer_status": "Completed",
-#                     "created_at": "",
-#                     "updated_at": "",
-#                     "items": [
-#                         {
-#                             "item_id": "P007435",
-#                             "amount": 23
-#                         }
-#                     ]
-#                 }
-#             case "warehouses":#todo
-#                 post_objects[obj] = {"id": 999, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "", "updated_at": ""}
-
-#     for file in files: #now add the items and test
-#         #pdb.set_trace()
-#         #print("file", file)
-#         #print("obj name", file.split(".")[0])
-#         obj_to_post = post_objects[file.split(".")[0]]
-#         #print("object_to_post", obj_to_post)
-#         start = timer()
-#         shutil.copyfile("data/" + file, "testing/test_data_backup/" + file) #backup original file
-#         response = requests.post(address + "/" + file.split(".")[0], #remove .json from filename
-#                                 json=obj_to_post,
-#                                 headers=
-#                                 {'API_KEY': 'a1b2c3d4e5',
-#                                 'content-type': 'application/json'}
-#                                 )
-#         response_time = timer() - start
-#         with open("data/" + file) as json_data:
-#             #response_data = response.json()
-#             db_data = json.load(json_data)
-#             db_obj = {}
-#             if file == "items.json":
-#                 for obj in db_data:
-#                     if obj.get("uid", -1) == obj_to_post.get("uid", -2):
-#                         db_obj = obj
-#             else:
-#                 for obj in db_data:
-#                     if obj.get("id", -1) == obj_to_post.get("id", -2):
-#                         db_obj = obj
-#             #pdb.set_trace()
-#             #print("object_to_post", obj_to_post)
-#             #print("db_obj", db_obj)
-#             success = remove_timestamps(db_obj) == obj_to_post
-#             #likely fails because POST updated the created_at and updated_at parameters
-#             #AND/OR __contains__ compares identity which would return false even if parameters are the same
-
-#             # print(response_data) run "python run_tests.py -s" in terminal to enable print
-#             # success = response_data == db_data
-#             results_file_name = "POST_" + file.split(".")[0]# + "_" + test_datetime
-#             diagnostics[results_file_name] = {"succes" : success, "response_time" : response_time}
-            
-#             with open(f"testing/results/POST/{test_datetime}/{results_file_name}.json", "w") as f:
-#                 json.dump(diagnostics[results_file_name], f)
-
-#             shutil.copyfile("testing/test_data_backup/" + file, "data/" + file) #restore file
-#             #assert response.ok
-#             #assert response.status_code == 201
-#             #assert success
-            
 
 def test_post_client():
     test_post_one_endpoint("clients.json")
@@ -236,18 +76,18 @@ def test_post_one_endpoint(file: string):
     results_file_name = f"POST_{file.split(".")[0]}_{test_datetime}" 
     os.makedirs(f"testing/results/POST", exist_ok=True)
 
-    match file.split(".")[0]:
-        case "clients":#todo
+    match file.split(".")[0]: #choose which object to add
+        case "clients":
             obj_to_post = {"id": 999, "name": "Test_Client", "address": "Nowhere", "city": "Nowhere", "zip_code": "00000", "province": "Nowhere", "country": "Nowhere", "contact_name": "Test_Client", "contact_phone": "242.732.3483x2573", "contact_email": "test_client@example.net", "created_at": "", "updated_at": ""}
-        case "inventories":#todo
+        case "inventories":
             obj_to_post = {"id": 999, "item_id": "P000001", "description": "Face-to-face clear-thinking complexity", "item_reference": "sjQ23408K", "locations": [3211, 24700, 14123, 19538, 31071, 24701, 11606, 11817], "total_on_hand": 262, "total_expected": 0, "total_ordered": 80, "total_allocated": 41, "total_available": 141, "created_at": "", "updated_at": ""}
-        case "item_groups":#todo
+        case "item_groups":
             obj_to_post = {"id": 999, "name": "Electronics", "description": "", "created_at": "", "updated_at": ""}
-        case "item_lines":#todo
+        case "item_lines":
             obj_to_post = {"id": 999, "name": "Tech Gadgets", "description": "", "created_at": "", "updated_at": ""}
-        case "item_types":#todo
+        case "item_types":
             obj_to_post = {"id": 999, "name": "Laptop", "description": "", "created_at": "", "updated_at": ""}
-        case "items":#todo
+        case "items":
             obj_to_post = {
                 "uid": "999",
                 "code": "sjQ23408K",
@@ -268,9 +108,9 @@ def test_post_one_endpoint(file: string):
                 "created_at": "",
                 "updated_at": ""
             }
-        case "locations":#todo
+        case "locations":
             obj_to_post = {"id": 999, "warehouse_id": 1, "code": "A.1.0", "name": "Row: A, Rack: 1, Shelf: 0", "created_at": "", "updated_at": ""}
-        case "orders":#todo
+        case "orders":
             obj_to_post = {
                 "id": 999,
                 "source_id": 33,
@@ -292,9 +132,9 @@ def test_post_one_endpoint(file: string):
                 "total_surcharge": 77.6,
                 "created_at": "",
                 "updated_at": "",
-                "items": [] #todo
+                "items": []
             }
-        case "shipments":#todo
+        case "shipments":
             obj_to_post=  {
                 "id": 999,
                 "order_id": 1,
@@ -314,11 +154,11 @@ def test_post_one_endpoint(file: string):
                 "total_package_weight": 594.42,
                 "created_at": "",
                 "updated_at": "",
-                "items": [] #todo
+                "items": [] 
             }
-        case "suppliers":#todo
+        case "suppliers":
             obj_to_post = {"id": 999, "code": "SUP0001", "name": "Lee, Parks and Johnson", "address": "5989 Sullivan Drives", "address_extra": "Apt. 996", "city": "Port Anitaburgh", "zip_code": "91688", "province": "Illinois", "country": "Czech Republic", "contact_name": "Toni Barnett", "phonenumber": "363.541.7282x36825", "reference": "LPaJ-SUP0001", "created_at": "", "updated_at": ""}
-        case "transfers":#todo
+        case "transfers":
             obj_to_post = {
                 "id": 999,
                 "reference": "TR00001",
@@ -334,12 +174,9 @@ def test_post_one_endpoint(file: string):
                     }
                 ]
             }
-        case "warehouses":#todo
+        case "warehouses":
             obj_to_post = {"id": 999, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "", "updated_at": ""}
 
-
-    #obj_to_post = post_objects[file.split(".")[0]]
-    #print("object_to_post", obj_to_post)
     start = timer()
     shutil.copyfile("data/" + file, "testing/test_data_backup/" + file) #backup original file
     response = requests.post(address + "/" + file.split(".")[0], #remove .json from filename
@@ -348,35 +185,29 @@ def test_post_one_endpoint(file: string):
                             {'API_KEY': 'a1b2c3d4e5',
                             'content-type': 'application/json'}
                             )
-    response_time = timer() - start
+    response_time = timer() - start 
+
+    #obj_to_post has "created_at" and "updated_at" properties set to ""
+    #when obj_to_post is posted using a POST call, our codebase adds a value to "created_at" and "updated_at"
+    #we thus have to find the object in the database using only it's Id, then set these properties back to "" to be able to compare
     with open("data/" + file) as json_data:
-        #response_data = response.json()
         db_data = json.load(json_data)
-        db_obj = {}
-        if file == "items.json":
+        db_obj = {} #makes sure it's empty rather than null if obj isn't found, otherwise remove_timestamps() throws exception
+        if file == "items.json": #because items uses a string uid rather than an int id
             for obj in db_data:
-                if obj.get("uid", -1) == obj_to_post.get("uid", -2):
+                if obj.get("uid", -1) == obj_to_post.get("uid", -2): # -1 and -2 as non-equal default return values in case of fail
                     db_obj = obj
         else:
             for obj in db_data:
                 if obj.get("id", -1) == obj_to_post.get("id", -2):
                     db_obj = obj
-        #pdb.set_trace()
-        #print("object_to_post", obj_to_post)
-        #print("db_obj", db_obj)
         success = remove_timestamps(db_obj) == obj_to_post
-        #likely fails because POST updated the created_at and updated_at parameters
-        #AND/OR __contains__ compares identity which would return false even if parameters are the same
-
-        # print(response_data) run "python run_tests.py -s" in terminal to enable print
-        # success = response_data == db_data
-        # + "_" + test_datetime
         diagnostics = {}
         diagnostics[results_file_name] = {"succes" : success, "response_time" : response_time}
 
         with open(f"testing/results/POST/{results_file_name}.json", "w") as f:
             json.dump(diagnostics[results_file_name], f)
 
-        shutil.copyfile("testing/test_data_backup/" + file, "data/" + file) #restore file
+        shutil.copyfile("testing/test_data_backup/" + file, "data/" + file) #restore file from backup
 
         assert success
