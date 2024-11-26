d6e3c83303c0d04a0836e5c6ea8e5bfe217cd1e6
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-11-26 10:19:33 +0100

    RunCargoHub.sh now succesfully kills old process and starts new one. Added deletion of old codebase to MigrateToServer workflow

--- RunCargoHub.sh
+++ RunCargoHub.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/bash
+
+#terminate if already active
+kill $(pgrep -f "dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj")
+#Re-run API
+nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj > /dev/null 2>&1 &
+
 #dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj # this works but isn't on background
-nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj &
 
 #CargoHub/bin/Debug/net8.0/CargoHub.exe
 #C:\VSCodeProjects\CargoHub\CargoHub\bin\Debug\net8.0\CargoHub.exe
