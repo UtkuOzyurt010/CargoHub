8c7bab59f9ebe079110334dc9874f3436a9c7e21
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-11-26 14:03:32 +0100

    changed url from localhost to 0.0.0.0

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -15,7 +15,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.WebHost.UseUrls("http://localhost:8000");
+        builder.WebHost.UseUrls("http://0.0.0.0:8000");
 
 
         builder.Services.AddControllers();
