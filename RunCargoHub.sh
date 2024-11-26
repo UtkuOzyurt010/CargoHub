c71db43f4e28d43ac007d0e4d4cf8cc1d4a710df
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-11-26 09:25:38 +0100

    permission fix .exe and try to run in background

--- RunCargoHub.sh
+++ RunCargoHub.sh
@@ -1,2 +1,6 @@
 #! /usr/bin/bash
-dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj
+#dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj # this works but isn't on background
+nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj &
+
+#CargoHub/bin/Debug/net8.0/CargoHub.exe
+#C:\VSCodeProjects\CargoHub\CargoHub\bin\Debug\net8.0\CargoHub.exe
