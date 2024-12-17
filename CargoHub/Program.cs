b852b97c3be64dedde151eefd64fb46e1886768d
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-12-17 13:37:11 +0100

    actually change port to 8080

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -15,7 +15,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.WebHost.UseUrls("http://0.0.0.0:80");
+        builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 
         builder.Services.AddControllers();
