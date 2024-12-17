db7fa9eac136282bf7623d858b4c33143d471591
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-12-17 13:27:24 +0100

    change port to 8080

--- RunCargoHub.sh
+++ RunCargoHub.sh
@@ -3,7 +3,9 @@
 #terminate if already active
 kill $(pgrep -f "dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj")
 #Re-run API
-echo "${{secrets.SERVERPASSWORD}}" | sudo nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj > /dev/null 2>&1 &
+nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj > /dev/null 2>&1 &
+
+#echo "${{secrets.SERVERPASSWORD}}" | sudo 
 
 #dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj # this works but isn't on background
 #CargoHub/bin/Debug/net8.0/CargoHub.exe
