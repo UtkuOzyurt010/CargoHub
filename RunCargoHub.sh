75affd5657a2d3fc796581f2d709c56b49f323e5
Utku Özyurt <87371120+UtkuOzyurt010@users.noreply.github.com>
2024-12-17 13:09:00 +0100

    removed 2 obsolete workflows. updated  RuncargoHub.sh script to use sudo nohup (to lfix port connection issue)

--- RunCargoHub.sh
+++ RunCargoHub.sh
@@ -3,9 +3,8 @@
 #terminate if already active
 kill $(pgrep -f "dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj")
 #Re-run API
-nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj > /dev/null 2>&1 &
+echo "${{secrets.SERVERPASSWORD}}" | sudo nohup dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj > /dev/null 2>&1 &
 
 #dotnet run --project /home/ubuntu-1079726/ubuntu-1079726/CargoHub/CargoHub/CargoHub.csproj # this works but isn't on background
-
 #CargoHub/bin/Debug/net8.0/CargoHub.exe
 #C:\VSCodeProjects\CargoHub\CargoHub\bin\Debug\net8.0\CargoHub.exe
